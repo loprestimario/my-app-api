819a0834f227ea87cfa6140057c305e0ff483d2d
Mario Lo Presti <mario84ct@hotmail.it>
2025-01-14 16:39:15 +0100

    first commit

--- index.js
+++ index.js
@@ -16,16 +16,9 @@ const corsOptions = {
 
 
 
-app.use((req, res, next) => {
-    res.header(
-        "Access-Control-Allow-Origin",
-        "React app URL"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    next();
-});
 
+
+app.use(cors(corsOptions));
 
 
 const db2 = mysql.createConnection({
